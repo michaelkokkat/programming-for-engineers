5e4e1f3da34190bcf2359988d8c1277c6f123c7a
Michael Leo Kokkat <54603696+michaelkokkat@users.noreply.github.com>
2019-08-28 15:25:58 +0400

    Update dist-between-pts.cpp

--- dist-between-pts.cpp
+++ dist-between-pts.cpp
@@ -4,22 +4,27 @@ using namespace std;
 
 int main()
 {
-	double x1,y1,x2,y2,side1,side2,distance;
+	double a, b, c, d, sideA, sideB, distance;
 
 		cout << "Enter the x-coordinate of the first point\n";
-		cin >> x1;
+		cin >> a;
+		
 		cout << "Enter the y-coordinate of the first point\n";
-		cin >> y1;
+		cin >> b;
+
 		cout << "Enter the x-coordinate of the second point\n";
-		cin >> x2;
+		cin >> c;
+
 		cout << "Enter the y-coordinate of the second point\n";
-		cin >> y2;
+		cin >> d;
 
-		side1 = x2 - x1;
-		side2 = y2 - y1;
-		distance = sqrt(side1 * side1 + side2 * side2);
+		sideA = c - a;
+		sideB = d - b;
 
-		cout << "The distance between the two points is " << distance << "\n";
+		distance = sqrt(sideA*sideA+sideB*sideB);
+
+		cout << "The distance between points ("<<a<<", "<<b<< ") and ("<< c<<", "<<d<<") is " << distance << ".\n";
+		
 		return 0;
 
 }
