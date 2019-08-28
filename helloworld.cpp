c99c8c93395d11e0b3936ba553d00d1a21f21028
Michael Leo Kokkat <54603696+michaelkokkat@users.noreply.github.com>
2019-08-28 15:39:29 +0400

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double bigD; //declared general use decimal numbers
+
+	cout << "Hello World!\n";
+	cout << "Enter a number.\n";
+	cin >> bigD;
+	bigD = bigD * 10;
+	cout << "You entered the number " << bigD << ".\n";
+
+	return 0;
+
+}
