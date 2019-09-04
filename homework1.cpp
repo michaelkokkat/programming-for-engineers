40567a3e6ff6d0270a619d74be13463c3d33b5f3
Michael Leo Kokkat <54603696+michaelkokkat@users.noreply.github.com>
2019-09-04 15:21:31 +0400

    Update homework1.cpp

--- homework1.cpp
+++ homework1.cpp
@@ -1,13 +1,20 @@
+/*****************************************/
+/* My Name, My Student Number */
+/* Sept 1, 2016. */
+/* This program prints out some messages: */
+/* on the screen. */
+/*****************************************/
+
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 int main()
 {
-	double x1, y1, x2, y2, sideA, sideB, slope, area, circum, perim;
+	double length, perm_load, var_load, k=0;
 
-	cout << "1. Calculating slope of a line when two points are given.\n";
-
+	cout << "In order to design a structurally sound rectangular concrete beam, enter the required specifications as prompted:\n";
+while (k!=1){
 
 	cout << "\nEnter the x-coordinate of the 1st point on the line: ";
 	cin >> x1;
@@ -31,49 +38,10 @@ int main()
 
 	cout << "2. Calculating area of triangle when base and height are given.\n";
 
-
-	cout << "\nEnter the base length of the triangle: ";
-	cin >> x1;
-
-	cout << "Enter the height of the triangle: ";
-	cin >> y1;
-
-
-	sideA = 0.5*x1*y1;
-
-	cout << "The area of the triangle is " << sideA << ".\n";
-
-	cout << "3. Calculating circumference and area of a circle when diameter is given.\n";
-
-
-	cout << "\nEnter the diameter of the circle: ";
-	cin >> x1;
-
-	sideA = 0.5*x1*0.5*x1*3.1415;
-	sideB = x1*3.1415;
-
-	cout << "The circumference of the circle is" << sideB << ", and the area is" << sideA << ".\n";
-
-	cout << "4. Calculating perimeter of a quadrilateral when side lengths are given.\n";
-
-
-	cout << "\nEnter the length of the first side: ";
-	cin >> x1;
-
-	cout << "Enter the length of the second side: ";
-	cin >> y1;
-
-	cout << "Enter the length of the third side: ";
-	cin >> x2;
-
-	cout << "Enter the length og the fourth side: ";
-	cin >> y2;
-
-
-	perim = x1 + x2 + y1 + y2;
-
-	cout << "The perimeter of the given quadrilateral is" << perim << ".\n";
-	system("pause");
+	cout << "Enter 1 to exit the program, or any other number to repeat: ";
+	cin >> k;
+}
+	
 	return 0;
 
 }
