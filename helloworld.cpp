6c6311c408459d9f2926e830e33e77b9c9febf5a
Michael Leo Kokkat <54603696+michaelkokkat@users.noreply.github.com>
2019-09-04 15:52:40 +0400

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,16 +2,30 @@
 #include <cmath>
 using namespace std;
 
-int main()
-{
-	double bigD; //declared general use decimal numbers
+int main() {
+	int a, b, sum, div, rem, incre, decre;
 
-	cout << "Hello World!\n";
-	cout << "Enter a number.\n";
-	cin >> bigD;
-	bigD = bigD * 10;
-	cout << "You entered the number " << bigD << ".\n";
+	cout << "Enter two integers separated by a space:\n";
+	cin >> a >> b;
 
-	return 0;
+	sum = a + b;
+	div = a / b;
+	rem = a%b;
+	incre = a + b++;
+	decre = a-- - b;
+	a++;
+	b--;
+	a *= b;
+	a %= b;
 
-}
+	cout << "1. Sum: " << sum << ".\n";
+	cout << "2. Quotient: " << div << ".\n";
+	cout << "3. Remainder: " << rem << ".\n";
+	cout << "4. (Abbrevited) Product: " << a << ".\n";
+	cout << "5. (Abbreviated) Remainder: " << b << ".\n";
+	cout << "6. Postfix Increment: " << incre << ".\n";
+	cout << "7. Prefix Decrement: " << decre << ".\n";
+	system("pause");
+		return 0;
+
+	}
