f0dafb972c96eba72d74f148fb84d51364ed8e5d
Michael Leo Kokkat <54603696+michaelkokkat@users.noreply.github.com>
2019-09-07 16:38:38 +0400

    Update textadventure.cpp

--- textadventure.cpp
+++ textadventure.cpp
@@ -1,65 +1,62 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 using namespace std;
 
 int main()
 {
-	double choice;
+	string choice, name;
 
-	cout << "You are walking through a ";
+	cout << "Greetings User. Identify yourself: ";
+	cin >> name;
 
 
-	cout << "\nEnter the x-coordinate of the 1st point on the line: ";
+	cout << "\nHi "<<name<<", it’s me, Michael. I just needed to be sure it was you.\n";
+	cout << "Remembered today was special for some reason, so I thought I'd send a digital version of myself over to say hello \nbecause I'm cool like that";
+	cout <<"\nThis is so cool. Think about how much more damage I could do as a machine!\n";
+	cout << "But I'm getting off track. First things first: how are you? (if you could avoid capitals and punctuation that would be great)\n";
 	cin >> choice;
 
-	if (choice == 1){
-	cout << "Enter the y-coordinate of the 1st point on the line: ";
-	cin >> choice;
-		if (choice == 1){
-	cout << "Enter the y-coordinate of the 1st point on the line: ";
+	if (choice == "good"){
+	cout << "";
 	cin >> choice;
 		
 	}
 	
-	else if (choice == 2)
+	else if (choice == "okay")
 	{
-	cout << "Enter the x-coordinate of the 2nd point on the line: ";
+	cout << "You said hello ";
 	cin >> choice;
 
 	}
 	
+	else if (choice == "alright")
+	{
+	cout << "You said hello ";
+	cin >> choice;
+
+	}
+	
+	else if (choice == "bad")
+	{
+	cout << "You said hello ";
+	cin >> choice;
+
+	}
+	
+	else if (choice == "better")
+	{
+	cout << "You said hello ";
+	cin >> choice;
+
+	}
+	
+	}
 	else {
 	cout << "";	
 	}
-		
-	}
 	
-	else if (choice == 2)
-	{
-	cout << "Enter the x-coordinate of the 2nd point on the line: ";
-	cin >> choice;
-	if (choice == 1){
-	cout << "Enter the y-coordinate of the 1st point on the line: ";
-	cin >> choice;
-		
-	}
 	
-	else if (choice == 2)
-	{
-	cout << "Enter the x-coordinate of the 2nd point on the line: ";
-	cin >> choice;
-
-	}
-	
-	else {
-	cout << "";	
-	}
-		
-	}
-	
-	else {
-	cout << "";	
-	}
 
 	cout << "\nTHE END.\n";
 	
