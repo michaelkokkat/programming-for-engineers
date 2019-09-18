2e77ca8a8c1e570b12528c20d4c3fc26870491e3
Michael Leo Kokkat <54603696+michaelkokkat@users.noreply.github.com>
2019-09-18 15:53:41 +0400

    Create timber.cpp

---
+++ 09/18/timber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int total_acres = 14000, year0 = 2500, growth_rate = 1.02, years;
+
+
+	for (int i = 1; i <= years; i++)
+	{
+	
+	}
+      system("pause");
+			return 0;
+
+}
