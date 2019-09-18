89d9b4f7cf040fbae339023cca0a8712982a3774
Michael Leo Kokkat <54603696+michaelkokkat@users.noreply.github.com>
2019-09-18 12:24:10 +0400

    Create example1.cpp

---
+++ 09/18/example1.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int count = 10;
+	while (count > 0)
+	{
+		cout << count << endl;
+		count = count - 1;
+	}
+
+	return 0;
+
+}
